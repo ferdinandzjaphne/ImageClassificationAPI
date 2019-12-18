e9b6a0da8f66dbe7657ee3743ceb2ff943f3864b
ferdinandz.japhne <aanjaphne1@gmail.com>
2019-12-18 17:31:29 +0700

    cleanup

--- app.py
+++ app.py
@@ -4,71 +4,71 @@ app = Flask(__name__)
 import numpy as np
 import os
 # import cv2
-import tensorflow as tf
-import keras
-from keras.models import Sequential
-from keras.layers import Conv2D, MaxPooling2D
-from keras.layers import Activation, Dropout, Flatten, Dense
-from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
-import base64
-from keras.models import load_model
-from PIL import Image
-from io import BytesIO
+# import tensorflow as tf
+# import keras
+# from keras.models import Sequential
+# from keras.layers import Conv2D, MaxPooling2D
+# from keras.layers import Activation, Dropout, Flatten, Dense
+# from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+# import base64
+# from keras.models import load_model
+# from PIL import Image
+# from io import BytesIO
 
-def predict_image_class(image):
-    model = Sequential()
-    model.add(Conv2D(32, (3, 3), input_shape=(100, 100, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+# def predict_image_class(image):
+#     model = Sequential()
+#     model.add(Conv2D(32, (3, 3), input_shape=(100, 100, 3)))
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(32, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+#     model.add(Conv2D(32, (3, 3)))
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Conv2D(64, (3, 3)))
-    model.add(Activation('relu'))
-    model.add(MaxPooling2D(pool_size=(2, 2)))
+#     model.add(Conv2D(64, (3, 3)))
+#     model.add(Activation('relu'))
+#     model.add(MaxPooling2D(pool_size=(2, 2)))
 
-    model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
-    model.add(Dense(64))
-    model.add(Activation('relu'))
-    model.add(Dropout(0.5))
-    model.add(Dense(5))
-    model.add(Activation('sigmoid'))
-
-
-
-    model.compile(loss='binary_crossentropy',
-                optimizer='rmsprop',
-                metrics=['accuracy'])
-
-    model.load_weights('50_epochs.h5')
+#     model.add(Flatten())  # this converts our 3D feature maps to 1D feature vectors
+#     model.add(Dense(64))
+#     model.add(Activation('relu'))
+#     model.add(Dropout(0.5))
+#     model.add(Dense(5))
+#     model.add(Activation('sigmoid'))
 
 
-    image = image.resize((100,100))
 
-    x = img_to_array(image)
+#     model.compile(loss='binary_crossentropy',
+#                 optimizer='rmsprop',
+#                 metrics=['accuracy'])
 
-    x = x.reshape((1,) + x.shape)
-
-
-    classes = model.predict_classes(x)
-
-    prediction_result = classes.item(0)
+#     model.load_weights('50_epochs.h5')
 
 
-    if prediction_result == 0:
-        class_name = "hydro coco"
-    elif prediction_result == 1:
-        class_name = "mintz"
-    elif prediction_result == 2:
-        class_name = "morisca"
-    elif prediction_result == 3:
-        class_name = "pop mie"
-    elif prediction_result == 4:
-        class_name = "ultra milk" 
+#     image = image.resize((100,100))
 
-    return class_name
+#     x = img_to_array(image)
+
+#     x = x.reshape((1,) + x.shape)
+
+
+#     classes = model.predict_classes(x)
+
+#     prediction_result = classes.item(0)
+
+
+#     if prediction_result == 0:
+#         class_name = "hydro coco"
+#     elif prediction_result == 1:
+#         class_name = "mintz"
+#     elif prediction_result == 2:
+#         class_name = "morisca"
+#     elif prediction_result == 3:
+#         class_name = "pop mie"
+#     elif prediction_result == 4:
+#         class_name = "ultra milk" 
+
+#     return class_name
 
 @app.route('/', methods=['GET'])
 def return_response():
