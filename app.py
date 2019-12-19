2787a5f427ce32bafbd0d6ed77f20b1affe57b78
ferdinandz.japhne <aanjaphne1@gmail.com>
2019-12-19 19:21:03 +0700

    sync loss function

--- app.py
+++ app.py
@@ -37,8 +37,8 @@ def predict_image_class(image):
 
 
 
-    model.compile(loss='binary_crossentropy',
-                optimizer='rmsprop',
+    model.compile(loss='categorical_crossentropy',
+                optimizer='adam',
                 metrics=['accuracy'])
 
 
@@ -50,7 +50,7 @@ def predict_image_class(image):
 
     x = img_to_array(image)
 
-    # x = np.expand_dims(x, axis=0)
+    x = np.expand_dims(x, axis=0)
 
     x = x.reshape((1,) + x.shape)
 
